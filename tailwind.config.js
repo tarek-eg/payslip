2aba99186f31f3cb531b9a40c2d2d87d8c1ba8a7
Tarek Mostafa <tarek.mostafa@zalando.de>
2021-01-17 10:18:18 +0100

    update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
